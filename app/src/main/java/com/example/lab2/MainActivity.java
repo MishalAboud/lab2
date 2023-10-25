df54b9b069130953fca56d568f4d0ae8948facc9
Mishal <abou0296@algonquinlive.com>
2023-10-25 18:44:37 -0400

    lab6

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,183 +1,192 @@
 package com.example.lab2;
 
-import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.DatabaseUtils;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final List<TodoItem> todoList = new ArrayList<>();
-    private ListView todoListView;
-    private EditText todoEditText;
-    private SwitchCompat urgentSwitch;
-    private Button addTodoButton;
-    private TodoAdapter adapter;
+    private ImageView catImageView;
+    private ProgressBar progressBar;
+    private Bitmap currentCatImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Convert fields to local variables
-        todoListView = findViewById(R.id.todoListView);
-        todoEditText = findViewById(R.id.todoEditText);
-        urgentSwitch = findViewById(R.id.urgentSwitch);
-        addTodoButton = findViewById(R.id.addTodoButton);
+        catImageView = findViewById(R.id.catImageView);
+        progressBar = findViewById(R.id.progressBar);
 
-        // Load todos from the database
-        loadTodosFromDatabase();
+        catImageView.setImageResource(R.drawable.banner_image);
 
-        adapter = new TodoAdapter(todoList, this);
-        todoListView.setAdapter(adapter);
-
-        addTodoButton.setOnClickListener(v -> {
-            String todoText = todoEditText.getText().toString();
-            boolean isUrgent = urgentSwitch.isChecked();
-
-            // Add the todo to the database
-            addTodoToDatabase(todoText, isUrgent);
-
-            // Add the todo to the list
-            todoList.add(new TodoItem(todoText, isUrgent));
-            adapter.notifyDataSetChanged();
-            todoEditText.getText().clear();
-        });
-
-        todoListView.setOnItemLongClickListener((parent, view, position, id) -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setTitle("Do you want to delete this?");
-            builder.setMessage("The selected row is: " + position);
-            builder.setPositiveButton("Yes", (dialog, which) -> {
-                // Delete the todo from the database
-                deleteTodoFromDatabase(position);
-
-                todoList.remove(position);
-                adapter.notifyDataSetChanged();
-            });
-            builder.setNegativeButton("No", null);
-            builder.show();
-            return true;
-        });
-
-        // Print database information to the Log window for debugging
-        printCursor(getAllTodosFromDatabase());
     }
 
-    private void loadTodosFromDatabase() {
-        TodoDatabaseHelper dbHelper = new TodoDatabaseHelper(this);
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        String[] projection = {TodoDatabaseHelper.COLUMN_TEXT, TodoDatabaseHelper.COLUMN_URGENT};
-        Cursor cursor = db.query(TodoDatabaseHelper.TABLE_TODO, projection, null, null, null, null, null);
+    public class CatImages extends AsyncTask<String, Integer, String> {
+        private Bitmap currentCatImage;
 
-        if (cursor != null) {
-            cursor.moveToFirst();
-            while (!cursor.isAfterLast()) {
-                String todoText = cursor.getString(0);
-                boolean isUrgent = cursor.getInt(1) == 1;
-                todoList.add(new TodoItem(todoText, isUrgent));
-                cursor.moveToNext();
+        @Override
+        protected String doInBackground(String... strings) {
+            while (true) {
+                String imageUrl = fetchCatImageUrl();
+                if (imageUrl != null) {
+                    String imageId = extractImageIdFromUrl(imageUrl);
+                    if (imageId != null && doesImageExistLocally(imageId)) {
+                        // The image exists locally; load it
+                        currentCatImage = loadLocalCatImage(imageId);
+                    } else {
+                        // The image does not exist locally; download and save it
+                        currentCatImage = downloadCatImage(imageUrl);
+                        if (imageId != null && currentCatImage != null) {
+                            saveLocalCatImage(imageId, currentCatImage);
+                        }
+                    }
+                    publishProgress(0); // Show progress bar
+                    publishProgress(100); // Hide progress bar and update image
+                }
+
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        publishProgress(i);
+                        Thread.sleep(30);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-            cursor.close();
-        }
-
-        db.close();
-    }
-
-    private void addTodoToDatabase(String text, boolean isUrgent) {
-        TodoDatabaseHelper dbHelper = new TodoDatabaseHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(TodoDatabaseHelper.COLUMN_TEXT, text);
-        values.put(TodoDatabaseHelper.COLUMN_URGENT, isUrgent ? 1 : 0);
-
-        db.insert(TodoDatabaseHelper.TABLE_TODO, null, values);
-        db.close();
-    }
-
-    private void deleteTodoFromDatabase(int position) {
-        TodoDatabaseHelper dbHelper = new TodoDatabaseHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        // Assuming _id is the primary key starting from 1
-        db.delete(TodoDatabaseHelper.TABLE_TODO, TodoDatabaseHelper.COLUMN_ID + " = ?", new String[]{String.valueOf(position + 1)});
-        db.close();
-    }
-
-    private Cursor getAllTodosFromDatabase() {
-        TodoDatabaseHelper dbHelper = new TodoDatabaseHelper(this);
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-
-        String[] projection = {TodoDatabaseHelper.COLUMN_ID, TodoDatabaseHelper.COLUMN_TEXT, TodoDatabaseHelper.COLUMN_URGENT};
-        return db.query(TodoDatabaseHelper.TABLE_TODO, projection, null, null, null, null, null);
-    }
-
-    private void printCursor(Cursor cursor) {
-        Log.d("Database Info", "Number of Results in Cursor: " + cursor.getCount());
-        Log.d("Database Info", "Cursor Data:\n" + DatabaseUtils.dumpCursorToString(cursor));
-        cursor.close();
-    }
-
-
-    private static class TodoAdapter extends BaseAdapter {
-        private final List<TodoItem> list;
-        private final Context context;
-
-        public TodoAdapter(List<TodoItem> list, Context context) {
-            this.list = list;
-            this.context = context;
         }
 
         @Override
-        public int getCount() {
-            return list.size();
-        }
+        protected void onProgressUpdate(Integer... values) {
+            int progress = values[0];
+            progressBar.setProgress(progress);
 
-        @Override
-        public Object getItem(int position) {
-            return list.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-            if (view == null) {
-                view = LayoutInflater.from(context).inflate(R.layout.list_item_todo, parent, false);
+            if (progress == 100) {
+                catImageView.setImageBitmap(currentCatImage);
             }
-            TextView todoText = view.findViewById(R.id.todoTextView);
-            TodoItem todoItem = list.get(position);
-            todoText.setText(todoItem.getText());
-            if (todoItem.isUrgent()) {
-                view.setBackgroundColor(Color.RED);
-                todoText.setTextColor(Color.WHITE);
-            }
-            return view;
         }
+
+        private String fetchCatImageUrl() {
+            try {
+                URL url = new URL("https://cataas.com/cat?json=true");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(5000); // Set a timeout (adjust as needed)
+                connection.connect();
+
+                if (connection.getResponseCode() == 200) {
+                    InputStream inputStream = connection.getInputStream();
+                    Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+                    if (scanner.hasNext()) {
+                        String json = scanner.next();
+                        JSONObject jsonObject = new JSONObject(json);
+                        String imageUrl = jsonObject.optString("url", null);
+                        if (imageUrl != null) {
+                            return "https://cataas.com/cat" + imageUrl; // Add the base URL
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("CatApp", "Error in fetchCatImageUrl (IO): " + e.getMessage());
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("CatApp", "Error in fetchCatImageUrl (JSON parsing): " + e.getMessage());
+            }
+
+
+            return null;
+        }
+
+
+        private String extractImageIdFromUrl(String imageUrl) {
+            try {
+                URI uri = new URI(imageUrl);
+                String path = uri.getPath();
+                String[] pathSegments = path.split("/");
+                return pathSegments[pathSegments.length - 1];
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                Log.e("CatApp", "Error in extractImageIdFromUrl: " + e.getMessage());
+            }
+
+            return null;
+        }
+
+
+        private boolean doesImageExistLocally(String imageId) {
+            try {
+                openFileInput("cat_image_" + imageId + ".png");
+                return true;
+            } catch (FileNotFoundException e) {
+                return false;
+            }
+        }
+
+
+        private Bitmap loadLocalCatImage(String imageId) {
+            try {
+                FileInputStream inputStream = openFileInput("cat_image_" + imageId + ".png");
+                return BitmapFactory.decodeStream(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("CatApp", "Error in loadLocalCatImage: " + e.getMessage());
+            }
+
+            return null;
+        }
+
+
+        private Bitmap downloadCatImage(String imageUrl) {
+            try {
+                URL url = new URL(imageUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(5000); // Set a timeout (adjust as needed)
+                connection.connect();
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = connection.getInputStream();
+                    Bitmap catImage = BitmapFactory.decodeStream(inputStream);
+                    return catImage;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("CatApp", "Error in downloadCatImage: " + e.getMessage());
+            }
+
+            return null;
+        }
+
+
+        private void saveLocalCatImage(String imageId, Bitmap catImage) {
+            try {
+                // Define a filename for the image using the imageId
+                String fileName = "cat_image_" + imageId + ".png";
+                FileOutputStream outputStream = openFileOutput(fileName, MODE_PRIVATE);
+                catImage.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("CatApp", "Error in saveLocalCatImage: " + e.getMessage());
+            }
+        }
+
     }
 }
